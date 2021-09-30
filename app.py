fceb4c32478c3606374022b8eef1ab9707184a02
siniauskii1 <66171894+siniauskii1@users.noreply.github.com>
2021-09-30 09:04:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,30 @@
 from flask import Flask, request
-from config import token as ttoken
 from config import *
 import time
-import telebot
-import threading
 import psycopg2
 conn = psycopg2.connect(dbname=db, user=user,
                         password=password, host=host)
 cursor = conn.cursor()
 app=Flask(__name__)
-bot = telebot.TeleBot(ttoken)
-Workers = {}
-
-с = 0
-
-@app.route('/start228')
-def test():
-    global с
-    if с == 0:
-      thr = threading.Thread(target=checker)
-      thr.start()
-      c = 1
-    return "OK"
 
 @app.route('/HealthChecker',methods = ['POST'])
 def testreq():
-    print("OKtest")
     if request.method=='POST':
         try:
             jsonStr = request.get_json()
             token = jsonStr['token']
             problem = jsonStr['problem']
-            Workers[token] = time.time()
-            cursor.execute(f"UPDATE Project SET isworking=1  WHERE token='{token}'")
+            cursor.execute(f"UPDATE Project SET isworking=1,lastupdate='{time.time()}'  WHERE token='{token}'")
             conn.commit()
         except Exception as err:
             print(err)
-
     return "Ok"
 
-
-def checker():
-    print("Okcheck")
-    while True:
-        try:
-            copy = Workers.copy().items()
-            for a in copy:
-                if float(time.time())-float(a[1])>10:
-                    #БАЗА ДАННЫХ
-                    mytoken = a[0]
-                    cursor.execute(f"UPDATE Project SET isworking=0  WHERE token='{mytoken}'")
-                    conn.commit()
-                    cursor.execute(f"SELECT user_id FROM Users WHERE token='{mytoken}'")
-                    allusers = cursor.fetchall()
-                    cursor.execute(f"SELECT projectname FROM Project WHERE token='{mytoken}'")
-                    projectname = cursor.fetchone()[0]
-                    qq = []
-                    for i in allusers:
-                        qq.append(i[0])
-                    print("Users", str(qq))
-                    del Workers[a[0]]
-                    for b in qq:
-                        try:
-                            bot.send_message(b,"Ваш сервер, под названием "+projectname+" упал")
-                        except:
-                            pass
-        except Exception as err:
-            print(err)
-
-
-
 def server():
-    app.run()
+    app.run(port=8080)
 
 
 
 if __name__ == '__main__':
-    print("okmain2")
-    thr = threading.Thread(target=server)
-    thr.start()
-    print("okmain")
-    checker()
-
+    server()
 
